514792ea167d73fd6e64fd5dffb492a2f05953f7
azizyildiz4257 <azizyildiz4257@hotmail.com>
2021-04-25 22:29:47 +0300

    25.04.2021

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager kredimanager , List<ILoggerService> loggerService)
+        {
+            kredimanager.Hesapla();
+            foreach (var logger in loggerService)
+            {
+                logger.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> Krediler)
+        {
+            foreach (var kredi in Krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interface lerde kendisini implemente eden classın referans numarasını tutabilir.
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            ıhtiyacKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            konutKrediManager.Hesapla();
+
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            tasıtKrediManager.Hesapla();
+
+            //Başvuru yapma simülasyonu
+            ILoggerService datebaseloggerservice = new DatabaseLoggerService();
+            ILoggerService fileloggerservice = new FileLoggerService();
+
+            /*List<ILoggerService> loggerServices = new List<ILoggerService>() { datebaseloggerservice, fileloggerservice };*/ 
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            Console.WriteLine();
+            basvuruManager.BasvuruYap(new EsnafKrediManager()/*alternatif instance*/, new List<ILoggerService>() { datebaseloggerservice, fileloggerservice });
+            Console.WriteLine();
+
+            //Kredi Seçimi simülasyon
+            List<IKrediManager> kredis = new List<IKrediManager>() { ıhtiyacKrediManager, konutKrediManager };
+            basvuruManager.KrediOnBilgilendirmesiYap(kredis);
+
+            Console.Read();
+        }
+    }
+}
